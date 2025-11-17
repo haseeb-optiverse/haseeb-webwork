d9078aaaf4933e96452194edee6147eca2b2b7f7
haseeb-optiverse <haseebff049@gmail.com>
2025-11-17 09:05:12 +0500

    javaScript methods,arrays

---
+++ javaScript/index.js
@@ -0,0 +1,151 @@
+/*
+let x = 5;
+
+let y = 6;
+
+console.log("z =", x + y);
+*/ 
+
+/*let fullName = "Haseeb";
+
+console.log(fullName);
+
+
+let age = "17";
+console.log(age);
+
+let city = "Layyah";
+console.log(city);
+
+let student ={
+    fullName: 'haseeb',
+    age: '17',
+    city: 'Layyah',
+}
+console.log(student);
+  
+
+
+
+//Variables
+
+var x = 30;{
+
+ let x = 10;
+   x = 15;
+console.log(x);
+
+
+
+
+{
+const x = 16;
+
+console.log(x);
+}
+}
+console.log(x); 
+*/
+
+
+       
+
+
+
+
+// Loops 
+ 
+// For loops
+ /*for (let i = 0; i <= 8; i++){
+  console.log("Number", i);
+} 
+  
+let sum = 0;
+
+for (let i = 1; i <= 6; i++){
+  sum = sum + 1;
+}
+ console.log("sum =",sum);
+ */
+
+ // While loop
+ /*
+ let i = 1;
+ while(i <=5){
+  console.log("i =", i);
+  i++;
+ }*/
+
+  //do-While loop
+  /*
+  let i = 20;
+  do{
+    console.log("i =", i);
+  } while(i <= 10);  */
+
+  // for of loop
+/*
+  let str = "Haseeb";
+
+let size = 0;
+  for(let val of str){
+    console.log("val =", val);
+    size++;
+  }  
+  console.log("Size =", size);*/
+
+
+  //for in loop
+
+ /* let student ={
+    name: "Haseeb",
+    age: 20,
+    cgpa:7.5,
+    isPass: true,
+  };
+ for (let key in student){
+     console.log(key, ":", student[key]);
+ } */
+
+
+     //Strings Method
+// toUpperCase
+/*
+    let str = "haseeb";
+   str = str.toUpperCase();
+    console.log(str); */
+
+// toLowerCase
+/*
+ let str = "HASEEB";
+ str = str.toLowerCase();
+ console.log(str); */
+
+ // trim method
+ /*
+ let str = "      My name is Haseeb    ";
+ console.log(str.trim()); */
+
+ //templete Literals  
+ /*
+ let obj ={
+  item:"pen",
+  price: 50,
+ };
+
+ let output =`The cost of ${obj.item} is ${obj.price} rupees`;
+ console.log(output); */
+
+ //Arrays
+ 
+ let fruits = ["Mango", "Banana", "Apple", "Orange"]; 
+  console.log(fruits);
+  console.log(fruits[2]); 
+   
+  //Array Methods 
+  /*
+  let fruits = ["Mango", "Banana", "Apple", "Orange"]; 
+
+  // push method
+  fruits.push("Grapes");
+  console.log(fruits); */
