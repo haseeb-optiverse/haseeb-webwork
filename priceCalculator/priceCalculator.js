40c2f02d0426a0cd67e2e43e3547335cbbd4020c
haseeb-optiverse <haseebff049@gmail.com>
2025-11-28 09:49:49 +0500

    priceCalculation

---
+++ priceCalculator/priceCalculator.js
@@ -0,0 +1,47 @@
+function getCalculation() {
+    let item = document.getElementById("item").value;
+    let quantity = Number(document.getElementById("quantity").value);
+
+    let price 
+    switch(item){
+        case "apple":
+            price = 20;
+            break;
+        case "milk":
+            price = 30;
+            break;
+        case "bread":
+            price = 40 ;
+            break;
+    }
+     
+    let total = price * quantity;
+
+    let discount 
+    if (quantity > 10 && total > 500 ){
+        discount = total * 0.20;
+    }
+    else if ( quantity > 10) {
+        discount = total * 0.10;
+    }
+    else if (total > 500) {
+        discount = total * 0.15;
+    }
+
+    let finalAmount = total - discount;
+
+    let message = discount > 0 ? `You Saved Rs : ${discount}` : "No discount applied.";
+
+    document.getElementById("output").innerHTML=
+    `
+    <p><b>Item :</b> ${item}</p>
+    <p><b>Quantity :</b> ${quantity}</p>
+    <p><b>Total :</b> ${total}</p>
+    <p><b>Discount :</b> ${discount}</p>
+    <p><b>Final Amount :</b> ${finalAmount}</p>
+    <hr>
+    <p><b>${message}</b></p>
+    `;
+}
+
+
